4ef53709387874054b2fc1915e7cb4b5f4ca11d8
Tzu-Ching Liao <tliao2@dt-login03.delta.ncsa.illinois.edu>
2026-07-05 17:19:05 -0500

    untrack scripts/run

--- scripts/run/clean_repo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This script deletes all .venv directories in the current directory and its subdirectories.
-find . -type d -name ".venv" -exec rm -rf {} +
-echo "All .venv directories have been deleted."
-
-find . -type d -name "__pycache__" -exec rm -rf {} +
-echo "All __pycache__ directories have been deleted."
